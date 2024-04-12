d366aadea08e099ee8396642da0ed8402065d37b
Ashish Chaudhary <chauashish21@gmail.com>
2024-04-12 20:48:54 +0530

    Added TextUtils

---
+++ textutils/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+<Navbar title="TextUtils"/>
+{/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
+
+    </>
+  );
+}
+
+export default App;
